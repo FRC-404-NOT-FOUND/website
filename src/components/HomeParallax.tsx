efb9db21e1010a7e8505779eeba21faa745035bb
Zachary <ztcollazo08@gmail.com>
2023-03-31 13:50:49 -0400

    Theming and a11y

--- src/components/HomeParallax.tsx
+++ src/components/HomeParallax.tsx
@@ -24,12 +24,13 @@ const calculateStartEnd = (lg: boolean, start: number, end: number) => {
 const HomeParallax = () => {
   const lg = useMediaQuery({ minWidth: 1024 })
   const dark = useMediaQuery({ query: "(prefers-color-scheme: dark)" })
+
   return (
-    <Parallax pages={6}>
+    <Parallax pages={7}>
       <ParallaxLayer offset={0} speed={1.5}>
         <Hero src={dark ? "hero-dark.png" : "hero-light.png"} />
       </ParallaxLayer>
-      <ParallaxLayer offset={0.5} speed={2.5} sticky={{ start: 1, end: lg ? 5 : 1.5 }}>
+      <ParallaxLayer offset={0.5} speed={2} sticky={{ start: 1, end: lg ? 5 : 1.5 }}>
         <Float>
           <Feature icon={MdInfo} title="About Us">
               <a href="/about" className="flex items-center">
@@ -61,9 +62,9 @@ const HomeParallax = () => {
       </ParallaxLayer>
       <ParallaxLayer offset={1} speed={1} sticky={calculateStartEnd(lg, 3.5, 4 )}>
         <Float dir="right">
-          <Feature icon={MdInfo} title="Safety... Dale?">
+          <Feature icon={MdInfo} title="Meet our Safety Mascot">
             <a href="/about/safety-dale" className="flex items-center">
-              Learn about our beloved king and embodiment of safety
+              Learn about Safety Dale and how we promote safety
               <MdChevronRight size={16} className="p-0" />
             </a>
           </Feature>
@@ -79,8 +80,17 @@ const HomeParallax = () => {
           </Feature>
         </Float>
       </ParallaxLayer>
-      <ParallaxLayer offset={5} speed={1}>
-        <div className="h-full absolute bottom-0 top-[59%] left-0 right-0">
+      <ParallaxLayer offset={5} speed={1.5} sticky={calculateStartEnd(lg, 5.5, 6)}>
+        <form action="/newsletter-signup" className="p-6 w-full flex flex-col gap-5 items-center justify-start">
+          <label className="text-3xl font-bold text-gradient" htmlFor="newsletter">Sign up for our Newsletter</label>
+          <div className="flex w-1/2 flex-row gap-3 items-center justify-center">
+            <input className="px-2 py-2 focus:outline-none w-full rounded-lg text-black focus:ring ring-offset-1 ring-red-700 dark:ring-violet-500" name="newsletter" placeholder="Email" />
+            <input className="button" type="submit" value="Sign Up" />
+          </div>
+        </form>
+      </ParallaxLayer>
+      <ParallaxLayer offset={6} speed={1} sticky={calculateStartEnd(lg, 6, 6.5)}>
+        <div className="h-full absolute bottom-0 top-[60%] left-0 right-0">
           <Footer />
         </div>
       </ParallaxLayer>
---
+++ src/components/StaticPage.tsx
@@ -0,0 +1,53 @@
+import React from "react"
+import { MdInfo, MdChevronRight } from "react-icons/md";
+import Feature from "./Feature";
+import Footer from "./Footer";
+import Hero from "./Hero";
+
+const StaticPage = () => (
+  <div>
+    <Hero />
+    <div className="container flex flex-col gap-10">
+      <Feature icon={MdInfo} title="About Us">
+          <a href="/about" className="flex items-center">
+            Learn more about our team and school
+            <MdChevronRight size={16} className="p-0" />
+          </a>
+      </Feature>
+      <Feature icon={MdInfo} title="Our History">
+        <a href="/about/history" className="flex items-center">
+          How it all started, and where it&apos;s going.
+          <MdChevronRight size={16} className="p-0" />
+        </a>
+      </Feature>
+      <Feature icon={MdInfo} title="Meet The Team">
+        <a href="/about/team" className="flex items-center">
+          See who&apos;s behind the magic.
+          <MdChevronRight size={16} className="p-0" />
+        </a>
+      </Feature>
+      <Feature icon={MdInfo} title="Meet our Safety Mascot">
+        <a href="/about/safety-dale" className="flex items-center">
+          Learn about Safety Dale and how we promote safety
+          <MdChevronRight size={16} className="p-0" />
+        </a>
+      </Feature>
+      <Feature icon={MdInfo} title="School">
+        <a href="/about/school" className="flex items-center">
+          Check out Appomattox Regional Governor&apos;s School
+          <MdChevronRight size={16} className="p-0" />
+        </a>
+      </Feature>
+      <form action="/newsletter-signup" className="p-6 w-full flex flex-col gap-5 items-center justify-start">
+        <label className="text-3xl font-bold text-gradient" htmlFor="newsletter">Sign up for our Newsletter</label>
+        <div className="flex flex-row gap-3 w-full items-center justify-center">
+          <input className="px-2 py-2 rounded-lg w-1/2 focus:outline-none text-black focus:ring ring-offset-1 ring-red-700 dark:ring-violet-500" name="newsletter" placeholder="Email" />
+          <input className="button" type="submit" value="Sign Up" />
+        </div>
+      </form>
+    </div>
+    <Footer />
+  </div>
+)
+
+export default StaticPage;
