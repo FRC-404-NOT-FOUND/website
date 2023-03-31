052d5ceeb3520d9e887d5332ec7a9146517615ef
Zachary <ztcollazo08@gmail.com>
2023-03-31 17:02:18 -0400

    Meet the team page

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,11 +1,11 @@
 import React, { ReactNode } from "react";
 import { IconType } from "react-icons";
 
-const Feature = ({ icon: Icon, title = 'Title', children }: { icon?: IconType, title?: string, children?: ReactNode }) => (
-  <div className="feature flex flex-col gap-4">
+const Feature = ({ icon: Icon, title, children, className }: { icon?: IconType, title?: string, children?: ReactNode, className?: string }) => (
+  <div className={`feature flex flex-col gap-4 ${className}`}>
     {Icon && <Icon className="react-icon" size={64} />}
     <div>
-      <h3>{title}</h3>
+      {title && <h3>{title}</h3>}
       <p>
         {children}
       </p>
